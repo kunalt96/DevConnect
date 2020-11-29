4126cbc927fa3d7e0eece374cf4790993cd5d326
Kunal Tiwari <kunaltiwari5596@gmail.com>
2020-11-29 16:00:23 +0530

    Inital Project Setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.send('API running')
+})
+
+app.listen(PORT,()=>{
+    console.log('Server Started at 5000')
+})
