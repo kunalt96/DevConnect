26ae1c523a24e6b94950210c18842404b7271c38
Kunal Tiwari <kunaltiwari5596@gmail.com>
2020-12-13 21:30:01 +0530

    Post model, Route for adding post is done

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator/check');
 
 // @route GET api/posts
 // @desc  Test route
 // @acess Public
-router.get('/',(req,res)=>{
-    res.send('From ROutess....POST')
-})
+router.post(
+  '/',
+  [auth, check('text', 'Text is required').not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      console.log('Hey');
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+      };
+      const post = await Post.create(newPost);
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,7 +34,8 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if (!errors) return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
     const {
       company,
       website,
