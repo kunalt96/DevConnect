fbe5c204a70abe3b43ef8e976d764bbdfe40d040
Kunal Tiwari <kunaltiwari5596@gmail.com>
2020-12-15 00:03:47 +0530

    Basic structure of Login implemented

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,52 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login Here</div>;
+  const [loginData, setLoginData] = useState({ email: '', password: '' });
+
+  const { email, password } = loginData;
+
+  const onChange = (e) =>
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    alert(`Email id is ${email} & password is ${password}`);
+  };
+
+  return (
+    <Fragment>
+      <h1 class='large text-primary'>Sign In</h1>
+      <p class='lead'>
+        <i class='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form class='form' onSubmit={(e) => onLogin(e)}>
+        <div class='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div class='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            required
+            value={password}
+            onChange={(e) => onChange(e)}
+            name='password'
+          />
+        </div>
+        <input type='submit' class='btn btn-primary' value='Login' />
+      </form>
+      <p class='my-1'>
+        Don't have an account? <Link to='/'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 export default Login;
