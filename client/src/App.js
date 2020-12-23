ebb94a7b20e3cda4c3fb1c252b5aa9dce723ded3
Kunal Tiwari <kunaltiwari5596@gmail.com>
2020-12-23 23:27:13 +0530

    Protected dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -37,6 +39,7 @@ const App = () => {
               <React.Fragment>
                 <Route exact path='/register' component={Register} />
                 <Route exact path='/login' component={Login} />
+                <PrivateRoute exact path='/dashboard' component={Dashboard} />
               </React.Fragment>
             </Switch>
           </section>
