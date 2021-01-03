40b95d7f5ece892ec865f469391cac279965dec2
Kunal Tiwari <kunaltiwari5596@gmail.com>
2021-01-03 15:10:25 +0530

    Final changes to deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +18,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/post', require('./routes/api/post'));
+
+// Serve static client assests in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
