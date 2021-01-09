c8d60ae9b12f20447aa2bff078c54bf51db7d5bf
Kunal Tiwari <kunaltiwari5596@gmail.com>
2021-01-10 03:36:28 +0530

    some bugs fixed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,6 +39,7 @@ router.post(
       req.file.filename
     );
     cloudinary.v2.uploader.upload(imgPath, (err, result) => {
+      console.log(result, err);
       res.send(result.secure_url);
     });
   }
